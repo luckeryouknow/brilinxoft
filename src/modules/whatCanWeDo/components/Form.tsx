e1059c62c9079d316a204bf8a93394efd412e603
Ruslan Vorozhbyt <vorozhbytr21@gmail.com>
2024-03-19 22:06:13 +0200

    added for styles

--- src/modules/whatCanWeDo/components/Form.tsx
+++ src/modules/whatCanWeDo/components/Form.tsx
@@ -3,7 +3,6 @@
 import Input from "./Input";
 import Select from 'react-select';
 import { useFormik } from "formik";
-import { useId } from "react";
 import * as Yup from 'yup';
 
 
@@ -19,8 +18,8 @@ export default function Form () {
     validationSchema: Yup.object().shape({
       name: Yup.string().required(),
       email: Yup.string().email().required(),
-      companyName: Yup.string().required(),
-      service: Yup.string().required(),
+      companyName: Yup.string(),
+      service: Yup.string(),
       tellMore: Yup.string(),
     }),
     onSubmit: (values) => {
@@ -30,7 +29,7 @@ export default function Form () {
         "company name": values.companyName,
         "service": values.service,
         "tell more": values.tellMore,
-      })
+      });
     },
   });
 
@@ -45,20 +44,21 @@ export default function Form () {
   ]
 
   return (
-    <form onSubmit={formik.handleSubmit} className="pb-[120px] overflow-hidden xl:w-1/2">
-      <div className="mb-8">
+    <form onSubmit={formik.handleSubmit} className="mt-10 pb-[120px] overflow-hidden xl:w-1/2 md:mt-0">
+      <div className="flex flex-col gap-4 mb-8">
         <Input formik={formik} id={"name"} placeholder="Name" />
         <Input formik={formik} id={"email"} placeholder="Email" />
         <Input formik={formik} id={"companyName"} placeholder="Your company name" />
         <div className="relative py-4">
+          <label className={`absolute top-0 left-4 ${formik.values.service ? "block" : "hidden"} text-sm`} htmlFor="service">Service</label>
           <Select
-            instanceId={useId()}
+            instanceId={"service"}
             options={options} 
             unstyled={true} 
             placeholder={"The service you are interested in"}
             onChange={(option) => formik.setFieldValue("service", option?.label)}
             classNames={{
-              control: () => (`pt-3 pb-5 px-4 ${dropdownError ? "border-b-[#C60047]" : "border-b-black"} dark:${dropdownError ? "border-b-[#C60047]" : "border-b-white"} border-b-[1px] text-black dark:text-[#D9D9D9]`),
+              control: () => (`pt-3 pb-5 px-4 ${dropdownError ? "border-b-[#C60047]" : "border-b-black hover:border-b-[#3892F5]"} dark:${dropdownError ? "border-b-[#C60047]" : "border-b-white dark:hover:border-b-[#3892F5]"} border-b-[1px] text-black dark:text-[#D9D9D9]`),
               option: () => ("py-5"),
               menu: () => ("w-full bg-white dark:bg-black"),
               dropdownIndicator: () => (""),
@@ -67,12 +67,15 @@ export default function Form () {
               <p className="absolute right-0 bottom-0 text-[#C60047] text-xs">This field is necessary</p>
             )}
         </div>
-        <textarea
-          onChange={formik.handleChange}
-          id={"tellMore"}
-          className="block min-h-[80px] w-full bg-white dark:bg-black pt-8 px-4 outline-none border-b-black dark:border-b-white border-b-[1px] text-black dark:text-[#D9D9D9] placeholder-current" 
-          placeholder="Tell us more about your project"
-        />
+        <div className="relative">
+          <label className={`absolute top-0 left-4 ${formik.values.tellMore ? "block" : "hidden"} text-sm`} htmlFor="tellMore">Tell us more about your project</label>
+          <textarea
+            onChange={formik.handleChange}
+            id={"tellMore"}
+            className="block min-h-[80px] w-full bg-white dark:bg-black pt-7 px-4 outline-none border-b-black dark:border-b-white border-b-[1px] text-black dark:text-[#D9D9D9] placeholder-current" 
+            placeholder="Tell us more about your project"
+          />
+        </div>
         <button type="submit" className={`
         text-white mt-8 md:mt-10 max-w-fit px-12 py-[14px] rounded-[20px] bg-gradient-to-r from-[#3892F5FF] via-[#B519F4FF] to-[#F10BD0FF] hover:from-100% transition-all ease-out duration-500 uppercase`
         }>
--- src/modules/whatCanWeDo/components/Input.tsx
+++ src/modules/whatCanWeDo/components/Input.tsx
@@ -11,12 +11,13 @@ export default function Input ({ id, formik, placeholder }: Props) {
 
   return (
     <div className="relative py-4">
+      <label className={`absolute top-0 left-4 ${formik.values[id] ? "block" : "hidden"} text-sm`} htmlFor={id}>{placeholder}</label>
       <input 
         id={id}
         placeholder={placeholder} 
         onChange={formik.handleChange}
         value={formik.values.id}
-        className={`block w-full bg-white dark:bg-black pt-3 pb-5 px-4 outline-none ${error ? "border-b-[#C60047]" : "border-b-black"} dark:${error ? "border-b-[#C60047]" : "border-b-white"} border-b-[1px] text-black dark:text-[#D9D9D9] placeholder-current`} 
+        className={`block w-full bg-white dark:bg-black pt-3 pb-5 px-4 outline-none ${error ? "border-b-[#C60047]" : "border-b-black hover:border-b-[#3892F5]"} dark:${error ? "border-b-[#C60047]" : "border-b-white dark:hover:border-b-[#3892F5]"} border-b-[1px] text-black dark:text-[#D9D9D9] placeholder-current`} 
       />
       {error && (
         <p className="absolute right-0 bottom-0 text-[#C60047] text-xs">This field is necessary</p>
